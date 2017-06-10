cd378e0f42361e56a59b932ba4b07606b77f0489
Warren Fletcher <w.fletcherZA@gmail.com>
2017-06-10 16:30:36 +0200

    Initial commit, Started Note and Control Modules

---
+++ vhdl/control.v
@@ -0,0 +1,11 @@
+module control(
+input clk_100,
+input BTNL,
+input BTNR,
+input BTNN,
+input BTNS,
+input BTNC)
+
+parameter N = 'd12;
+reg []
+
---
+++ vhdl/note.v
@@ -0,0 +1,33 @@
+ module note(clk, LUT,o_up, o_down, enable, agg_ack, agg_out)
+  input clk, LUT, enable, agg_ack, o_up, o_down;
+  output agg_out;
+  reg [11:0]index;
+  reg [7:0]jump;
+  initial index = 'd0;
+  initial jump = 'd1;
+  always @(posedge o_up)
+    begin
+      jump = jump << 1;
+    end
+  always @(posedge o_down)
+    begin
+      jump = jump >> 1;
+      if (jump <= 0)
+        begin
+          jump = 1;
+        end
+    end
+  always @(posedge agg_ack)
+    begin
+      assign agg_out = 0;
+    end
+  always @(posedge clk)
+    begin
+      if (enable == 0)
+        begin
+        break;
+        end
+      assign agg_out = LUT[index];
+      index = index+jump;
+    end
+endmodule
